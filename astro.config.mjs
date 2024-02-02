73f412a02fdc97bbd629446f5e270503a8ca8fa0
jesus-rm <jesus_olimpo@outlook.com>
2024-02-02 17:40:35 -0600

    Creación de primera isla de Astro e incluyendo un cartel de felicitación de Preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://tutorialastro-jesusrm.netlify.app"
+  site: "https://tutorialastro-jesusrm.netlify.app",
+  integrations: [preact()]
 });
